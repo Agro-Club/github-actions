bf4568dfea8d0f64f73f3e24edb162a20d6c68ea
Nikita Sokolov <n.sokolov@agro.club>
2022-10-31 17:06:14 +0300

    ENT-2658 test 3

--- spec-group-generator/index.js
+++ spec-group-generator/index.js
@@ -6,8 +6,10 @@ const start = async () => {
     if (!count || isNaN(count) || !isFinite(count))
         throw new TypeError("count must be a number");
     const path = core.getInput("path", { required: true });
+    console.log(`Generating specs from ${path}...`);
     const resolvedPath = resolve(path);
     const direntArr = await readdir(resolvedPath, { withFileTypes: true });
+    console.log(`Found dirents:`, direntArr);
     const maxPerGroup = Math.ceil(direntArr.length / count);
     const specGroups = direntArr.reduce((acc, dirent, index) => {
         const groupIndex = Math.floor(index / maxPerGroup);
@@ -20,7 +22,7 @@ const start = async () => {
         return acc;
     }, []);
     core.setOutput("result", specGroups);
-    console.log("Found specs: ", specGroups);
+    console.log("Generated specs: ", specGroups);
     return specGroups;
 };
 start();
--- spec-group-generator/index.ts
+++ spec-group-generator/index.ts
@@ -9,9 +9,12 @@ const start = async () => {
     throw new TypeError("count must be a number");
 
   const path = core.getInput("path", { required: true });
+
+  console.log(`Generating specs from ${path}...`);
   const resolvedPath = resolve(path);
 
   const direntArr = await readdir(resolvedPath, { withFileTypes: true });
+  console.log(`Found dirents:`, direntArr);
 
   const maxPerGroup = Math.ceil(direntArr.length / count);
 
@@ -30,7 +33,7 @@ const start = async () => {
 
   core.setOutput("result", specGroups);
 
-  console.log("Found specs: ", specGroups);
+  console.log("Generated specs: ", specGroups);
 
   return specGroups;
 };
