58e1b694743b6061e3e2e6e7294f01eaaafdcbf4
Nikita Sokolov <n.sokolov@agro.club>
2022-11-01 17:25:50 +0300

    ENT-2658 test 16

--- spec-group-generator/index.js
+++ spec-group-generator/index.js
@@ -2,47 +2,59 @@ import * as core from "@actions/core";
 import glob from "glob";
 import { readFile } from "fs/promises";
 import { Parser } from "xml2js";
+const sortByTime = (a, b) => {
+    return a.estimatedTime - b.estimatedTime;
+};
 const start = async () => {
     const count = parseInt(core.getInput("count", { required: true }));
     if (!count || isNaN(count) || !isFinite(count))
         throw new TypeError("count must be a number");
+    let resultGroups = [];
     const testFiles = glob.sync(core.getInput("tests"));
     const resultsFiles = glob.sync(core.getInput("results"));
-    const parser = new Parser();
-    await Promise.all(resultsFiles.map(async (file) => {
-        const xmlString = await readFile(file, "utf8");
-        const parsed = (await parser.parseStringPromise(xmlString)).testsuites;
-        console.log(parsed.testsuite[0].$.file, parsed.$.time);
-    }));
-    //glob(resultsGlob, (err, files) => {
-    //  if (err) throw err;
-    //  resultsFiles = files;
-    //});
-    //const resolvedTestPath = resolve(__dirname, testPath);
-    //console.log(`Generating specs from ${resolvedTestPath}`);
-    //
-    //const direntArr = (
-    //  await readdir(resolvedTestPath, { withFileTypes: true })
-    //).filter((dirent) =>
-    //  dirent.isFile()
-    //    ? dirent.name.match(new RegExp(core.getInput("include")))
-    //    : true
-    //);
-    //
-    //const specGroups = direntArr.reduce<string[]>((acc, dirent, index) => {
-    //  const groupIndex = index % count;
-    //  const groupPath = dirent.isDirectory()
-    //    ? `${path}/${dirent.name}/*`
-    //    : `${path}/${dirent.name}`;
-    //
-    //  if (!acc[groupIndex]) acc[groupIndex] = groupPath;
-    //  else acc[groupIndex] = `${acc[groupIndex]}, ${groupPath}`;
-    //
-    //  return acc;
-    //}, []);
-    //
-    //core.setOutput("result", specGroups);
-    console.log("Generated specs: ", resultsFiles);
-    return testFiles;
+    if (testFiles.length === 0) {
+        console.log("No test files found");
+    }
+    else if (resultsFiles.length > 0) {
+        console.log("Results files found, generating optimized spec groups...");
+        const parser = new Parser();
+        const testToTime = {};
+        await Promise.all(resultsFiles.map(async (file) => {
+            const xmlString = await readFile(file, "utf8");
+            const parsed = (await parser.parseStringPromise(xmlString)).testsuites;
+            testToTime[parsed.testsuite[0].$.file] = parseFloat(parsed.$.time);
+        }));
+        const groups = new Array(count).fill(0).map(() => ({
+            value: "",
+            estimatedTime: 0,
+        }));
+        testFiles.forEach((file) => {
+            groups.sort(sortByTime);
+            const group = groups[0];
+            if (group.value)
+                group.value += `, ${file}`;
+            else
+                group.value = file;
+            groups[0].estimatedTime += testToTime[file];
+        });
+        resultGroups = groups.map((group) => group.value).filter(Boolean);
+        if (resultGroups.length < groups.length) {
+            console.log("Some empty groups has been removed!");
+        }
+        console.log("Generated spec groups: ", groups.map((group) => `${group.value} (~${group.estimatedTime}s)`));
+    }
+    else {
+        resultGroups = testFiles.reduce((acc, file, index) => {
+            const groupIndex = index % count;
+            if (!acc[groupIndex])
+                acc[groupIndex] = file;
+            else
+                acc[groupIndex] = `${acc[groupIndex]}, ${file}`;
+            return acc;
+        }, []);
+        console.log("Generated spec groups: ", resultGroups);
+    }
+    core.setOutput("groups", resultGroups);
+    return resultGroups;
 };
 start();
