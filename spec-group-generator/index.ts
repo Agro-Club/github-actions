fa15ffc854e8407308e87a9ac3da3bbac1d5fa5b
Nikita Sokolov <n.sokolov@agro.club>
2022-11-02 12:35:00 +0300

    ENT-2658 added more logging

--- spec-group-generator/index.ts
+++ spec-group-generator/index.ts
@@ -47,6 +47,11 @@ const start = async () => {
       if (group.value) group.value += `, ${file}`;
       else group.value = file;
       groups[0].estimatedTime += testToTime[file] || 0;
+      console.log(
+        `Added ${file} to group ${groups.indexOf(group)}, time: ${
+          testToTime[file] || 0
+        }`
+      );
     });
 
     resultGroups = groups.map((group) => group.value).filter(Boolean);
