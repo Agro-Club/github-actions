3dfab07521b9e0fdff55e2554361f1fe026a5a1a
Nikita Sokolov <n.sokolov@agro.club>
2022-11-09 20:00:43 +0300

    ENT-4079 test workflow web hooks 7

--- commit-messages-parser/index.js
+++ commit-messages-parser/index.js
@@ -1,40 +1,22 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
 async function start() {
-    const token = core.getInput("token");
     const regexp = new RegExp(core.getInput("regexp"));
-    const owner = core.getInput("owner");
-    const repo = core.getInput("repo");
-    const head = core.getInput("head", { required: true });
-    const base = core.getInput("base");
-    const octokit = github.getOctokit(token);
-    try {
-        const response = await octokit.rest.repos.compareCommits({
-            repo,
-            owner,
-            base: base || head,
-            head,
-            per_page: 100,
-        });
-        const entries = new Set([]);
-        for (const { commit } of response.data.commits) {
-            const match = commit.message.match(regexp);
-            if (match?.[0]) {
-                entries.add(match[0].toUpperCase());
-            }
+    const commits = JSON.parse(core.getInput("commits", { required: true }));
+    const entries = new Set([]);
+    for (const { commit } of commits) {
+        const match = commit.message.match(regexp);
+        if (match?.[0]) {
+            entries.add(match[0].toUpperCase());
         }
-        if (entries.size === 0) {
-            core.setOutput("entries", "");
-            console.log("Nothing found");
-            return entries;
-        }
-        const entriesArr = [...entries];
-        console.log(`Found entries: ${entriesArr}`);
-        core.setOutput("entries", entriesArr);
+    }
+    if (entries.size === 0) {
+        core.setOutput("entries", "");
+        console.log("Nothing found");
         return entries;
     }
-    catch (error) {
-        throw new Error(`Failed to load commit messages between ${head} and ${base}`);
-    }
+    const entriesArr = [...entries];
+    console.log(`Found entries: ${entriesArr}`);
+    core.setOutput("entries", entriesArr);
+    return entries;
 }
 start();
--- commit-messages-parser/index.ts
+++ commit-messages-parser/index.ts
@@ -1,55 +1,32 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
 
 async function start() {
-  const token = core.getInput("token");
-
   const regexp = new RegExp(core.getInput("regexp"));
-  const owner = core.getInput("owner");
-  const repo = core.getInput("repo");
-  const head = core.getInput("head", { required: true });
-  const base = core.getInput("base");
+  const commits = JSON.parse(core.getInput("commits", { required: true }));
 
-  //Sending graphql query to get the commit messages of the pull request
-  const octokit = github.getOctokit(token);
+  const entries: Set<string> = new Set([]);
 
-  try {
-    const response: any = await octokit.rest.repos.compareCommits({
-      repo,
-      owner,
-      base: base || head,
-      head,
-      per_page: 100,
-    });
-
-    const entries: Set<string> = new Set([]);
-
-    //Parsing commit messages
-    for (const { commit } of response.data.commits) {
-      const match = commit.message.match(regexp);
-      if (match?.[0]) {
-        entries.add(match[0].toUpperCase());
-      }
+  //Parsing commit messages
+  for (const { commit } of commits) {
+    const match = commit.message.match(regexp);
+    if (match?.[0]) {
+      entries.add(match[0].toUpperCase());
     }
-
-    //If no issue keys are found, we return an empty set
-    if (entries.size === 0) {
-      core.setOutput("entries", "");
-      console.log("Nothing found");
-      return entries;
-    }
-
-    const entriesArr = [...entries];
-
-    console.log(`Found entries: ${entriesArr}`);
-    core.setOutput("entries", entriesArr);
-
-    return entries;
-  } catch (error: any) {
-    throw new Error(
-      `Failed to load commit messages between ${head} and ${base}`
-    );
   }
+
+  //If no issue keys are found, we return an empty set
+  if (entries.size === 0) {
+    core.setOutput("entries", "");
+    console.log("Nothing found");
+    return entries;
+  }
+
+  const entriesArr = [...entries];
+
+  console.log(`Found entries: ${entriesArr}`);
+  core.setOutput("entries", entriesArr);
+
+  return entries;
 }
 
 start();
