c67724ad8541cfae3d299ec1201efb93dfe8b92d
Nikita Sokolov <n.sokolov@agro.club>
2022-11-20 13:04:30 +0300

    additional logging to delete artifacts

--- delete-artifacts/index.js
+++ delete-artifacts/index.js
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 const start = async () => {
     const nameRegexp = new RegExp(core.getInput("name", { required: true }), "gmi");
+    core.info(`==> Given regex ${nameRegexp}`);
     const owner = core.getInput("owner");
     const repo = core.getInput("repo");
     const octokit = github.getOctokit(core.getInput("token"));
@@ -10,7 +11,10 @@ const start = async () => {
         repo,
     });
     await Promise.all(res.data.artifacts
-        .filter(({ name }) => nameRegexp.test(name))
+        .filter(({ name }) => {
+        core.info(`==> Checking artifact ${name}`);
+        return nameRegexp.test(name);
+    })
         .map(({ id, name, url }) => {
         core.info(`==> Deleting artifact ${name} with url=${url}`);
         return octokit.rest.actions.deleteArtifact({
